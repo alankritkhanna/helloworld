9a8ce9ca7f33b2c174ea058e7b1bf1a9e256ca12
AK <abc@gmail.com>
2020-09-08 11:16:38 +0000

    5 times HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
+
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! from newfeature branch"); 
+        for(int i=0;i<5;i++){
+		System.out.println("Hello, World! from newfeature branch"); 
+    	}
     }
 }
